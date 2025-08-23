b9028e96f63211ea3c037ea9ea2ae36931ca41c6
muhammadzahid <muhammadzahid@mtechub.org>
2025-08-23 23:32:05 +0500

    add live db

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,22 @@
 // utils/db.js
 import { Pool } from "pg";
 
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     max: 20, // Max number of clients in the pool
+//     idleTimeoutMillis: 30000,
+//     connectionTimeoutMillis: 5000,
+// });
+
+// online db 
+
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    max: 20, // Max number of clients in the pool
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 5000,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // important for Neon
 });
 
 export default pool;
