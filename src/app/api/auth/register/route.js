b185fa4c179ee5b458f77b830cb68bb81e5c0b1f
Muhammad Haroon <haroontime437@gmail.com>
2025-10-12 22:38:19 -0700

    got remaining data from table in Register Api and also Email format validation

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -5,16 +5,32 @@ import bcrypt from "bcrypt"
 export async function POST(req) {
 
     try {
-        const { username, email, password } = await req.json();
+        const { username, email, password, bio } = await req.json();
 
-        if (!username || !email || !password) {
+        if (!username || !email || !password || !bio) {
             const missingField =
-                !username ? "Username is required" : !email ? "Email is required" : "Password is required";
+                !username ? "Username is required" : !email ? "Email is required" : !password ? "Password is required" : "bio is Required";
             return NextResponse.json({
                 message: missingField,
                 success: false
             }, { status: 404 })
         };
+
+        //  check the email format
+        const emailFormat=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailFormat.test(email)){
+            return NextResponse.json({
+                message:"Invalid email format. Please enter a valid email address.",
+                success:false
+            },
+            {status:400}
+        )
+            
+        }
+
+
+
+
         // if email is already exist
         const Isemail = await pool.query(`SELECT email FROM users WHERE email=$1`, [email]);
         if (Isemail.rows.length > 0) {
@@ -28,11 +44,16 @@ export async function POST(req) {
         const saltRounds = 10;
         const passwordHash = await bcrypt.hash(password, saltRounds);
 
+        // images 
+
+        const profole_images="https://i.pravatar.cc/150?img=3";
+        const  background_image="https://images.unsplash.com/photo-1503264116251-35a269479413";
+
 
         // query
-        const q = await pool.query(`INSERT INTO users(username,email,password)
-    VALUES($1,$2,$3) RETURNING *
-    `, [username, email, passwordHash]);
+        const q = await pool.query(`INSERT INTO users(username,email,password,bio,profole_images,background_image)
+    VALUES($1,$2,$3,$4,$5,$6) RETURNING *
+    `, [username, email, passwordHash, bio, profole_images,background_image]);
 
 
         return NextResponse.json({
